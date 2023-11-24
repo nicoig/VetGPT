2a4f1ea92d694009f6c94478b4fecdcf785a4b88
Nicolás Ignacio Guerra Villalobos <enguerra@Carozzicorp.com>
2023-11-24 10:52:05 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,40 +1,31 @@
-# DocGPT - Consulta Médica Virtual con Análisis de Imágenes
+# VetGPT
 
 ## Descripción
-DocGPT es una aplicación revolucionaria que utiliza modelos avanzados de inteligencia artificial para analizar imágenes y proporcionar información general sobre posibles afecciones médicas. La aplicación combina la potencia de la IA con un análisis de imágenes para ofrecer sugerencias de tratamiento y recomendaciones sobre qué tipo de profesional médico sería adecuado para una consulta específica.
+VetGPT es una aplicación de inteligencia artificial orientada a proporcionar consultas veterinarias virtuales. Utiliza tecnologías avanzadas de procesamiento de imágenes y generación de texto para analizar fotografías de mascotas y responder consultas relacionadas con su salud y bienestar.
 
-![DocGPT App Screenshot](img/app.jpg)
-
-## Funcionalidades
-- **Análisis de Imágenes Médicas**: Los usuarios pueden cargar imágenes relacionadas con sus consultas médicas, y la aplicación analizará estas imágenes utilizando el modelo de IA GPT-4 Vision.
-- **Diagnóstico Preliminar**: Basado en el análisis de la imagen y la descripción proporcionada por el usuario, la aplicación proporciona un diagnóstico preliminar.
-- **Sugerencias de Tratamiento**: Se generan recomendaciones generales de tratamiento utilizando el modelo GPT-3.5 Turbo.
-- **Recomendaciones de Especialistas Médicos**: La aplicación sugiere qué tipo de profesional médico podría ser más adecuado para tratar la afección identificada.
-- **Descarga de Resumen**: Los usuarios pueden descargar un resumen de la consulta, incluyendo el diagnóstico y las sugerencias de tratamiento.
-
-## Cómo Usar
-1. Cargar una imagen relacionada con la consulta médica.
-2. Describir la consulta o los síntomas en el campo de texto proporcionado.
-3. Presionar "Enviar Consulta" para que la aplicación procese la información.
-4. Leer el diagnóstico preliminar y las sugerencias de tratamiento.
-5. Consultar al profesional médico recomendado según sea necesario.
-6. Descargar el resumen de la consulta si se desea.
-
-## Instalación y Configuración
-
-Para ejecutar DocGPT, es necesario tener Python y Streamlit instalados. Además, se requieren claves API para utilizar los modelos de OpenAI.
-pip install streamlit
-pip install openai
+![VetGPT App Screenshot](img/app.jpg)
 
 
-Antes de ejecutar la aplicación, asegúrese de configurar las claves API en su entorno.
+## Características
+- **Análisis de Imágenes**: Identifica la raza del animal a partir de una imagen cargada y proporciona información relevante.
+- **Consultas Veterinarias**: Genera respuestas a preguntas específicas sobre la salud y el cuidado de las mascotas.
+- **Descarga de Información**: Permite a los usuarios descargar la información generada como un archivo de texto para su referencia.
 
-## Ejecución
-streamlit run app.py
+## Tecnologías Utilizadas
+- Streamlit
+- LangChain
+- OpenAI's GPT-3.5 y GPT-4 (para el análisis de texto y de imágenes)
+- Python
 
-## Nota Importante
-DocGPT es una herramienta de asistencia y no debe usarse como un sustituto de un diagnóstico médico profesional. Siempre se recomienda consultar a un profesional médico calificado para cualquier problema de salud.
+## Instalación y Uso
+Para utilizar VetGPT, sigue los siguientes pasos:
+
+1. Clona el repositorio a tu máquina local utilizando `git clone https://github.com/nicoig/VetGPT.git`
+2. Instala las dependencias necesarias utilizando `pip install -r requirements.txt`.
+3. Inicia la aplicación ejecutando `streamlit run app.py` en tu terminal.
+
+## Contribuciones
+Las contribuciones son siempre bienvenidas. Si tienes alguna sugerencia o mejora, no dudes en abrir un 'issue' o hacer un 'pull request'.
 
 ## Autor
-NicoIG
-
+@NicoIG
--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 # Primera Carga a Github
 # git init
 # git add .
-# git remote add origin https://github.com/nicoig/docGPT.git
+# git remote add origin https://github.com/nicoig/VetGPT.git
 # git commit -m "Initial commit"
 # git push -u origin master
 
@@ -16,6 +16,11 @@
 # En Render
 # agregar en variables de entorno
 # PYTHON_VERSION = 3.9.12
+
+# git remote set-url origin https://github.com/nicoig/VetGPT.git
+# git remote -v
+# git push -u origin main
+
 
 ################################################
 ##
@@ -38,16 +43,39 @@ load_dotenv(find_dotenv())
 def encode_image(image_file):
     return base64.b64encode(image_file.read()).decode('utf-8')
 
-# Configura el título y subtítulo de la aplicación en Streamlit
-st.title("DocGPT")
-st.subheader("Consulta médica virtual con análisis de imágenes")
-# Imagen
-st.image('img/robot.jpg', width=350)
+# Función para descargar información
+def download_button(object_to_download, download_filename, button_text):
+    buffer = BytesIO()
+    buffer.write(object_to_download.encode())
+    buffer.seek(0)
+    return st.download_button(
+        button_text,
+        buffer,
+        download_filename,
+        "text/plain"
+    )
 
+# Configura el título y subtítulo de la aplicación en Streamlit
+st.title("VetGPT")
+
+st.markdown("""
+    <style>
+    .small-font {
+        font-size:18px !important;
+    }
+    </style>
+    <p class="small-font">Hola, soy VetGPT y te ayudaré con dudas que tengas sobre tu mascota, carga tu imágen y te proporcionaré consejos e información sobre tu mascota</p>
+    """, unsafe_allow_html=True)
+
+# Imagen de cabecera
+st.image('img/robot.png', width=250)
 
 # Carga de imagen y texto por el usuario
-uploaded_file = st.file_uploader("Carga una imagen relacionada con tu consulta", type=["jpg", "png", "jpeg"])
-input_text = st.text_input("Describe tu consulta o síntomas aquí")
+uploaded_file = st.file_uploader("Carga una imagen de tu mascota", type=["jpg", "png", "jpeg"])
+input_text = st.text_input("Describe tu consulta veterinaria aquí")
+
+# Variable para almacenar la información generada
+informacion_generada = ""
 
 # Botón de enviar y proceso principal
 if st.button("Enviar Consulta") and uploaded_file is not None and input_text:
@@ -57,45 +85,41 @@ if st.button("Enviar Consulta") and uploaded_file is not None and input_text:
         # Analizar la imagen y el texto con la IA
         chain = ChatOpenAI(model="gpt-4-vision-preview", max_tokens=1024)
         msg = chain.invoke(
-            [AIMessage(content="Basándose en la imagen y la descripción proporcionada, identifique posibles afecciones relacionadas y proporcione información general."),
+            [AIMessage(content="Basándose en la imagen y la descripción proporcionada, identifique el animal y la raza, y proporcione información relevante."),
              HumanMessage(content=[{"type": "text", "text": input_text},
                                    {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{image}"}}])
             ]
         )
 
-        diagnostico = msg.content
-        st.markdown("**Información general basada en tu consulta:**")
-        st.write(diagnostico)
+        informacion_mascota = msg.content
+        informacion_generada += "**Información sobre tu mascota basada en tu consulta:**\n" + informacion_mascota + "\n\n"
 
-        # Generar recomendaciones de tratamiento
+        # Generar recomendaciones de cuidado para la mascota
         chain = ChatOpenAI(model="gpt-3.5-turbo", max_tokens=1024)
-        prompt_tratamiento = PromptTemplate.from_template(
+        prompt_cuidado = PromptTemplate.from_template(
             """
-            Dada la siguiente información general basada en una consulta médica:
-            {diagnosis}
-            ¿Qué recomendaciones generales se podrían dar? Estas son solo sugerencias generales y no deben reemplazar el asesoramiento médico profesional.
+            Dada la siguiente información sobre una mascota:
+            {pet_info}
+            
+            Basándose en la edad, raza y condición del animal, responda las siguientes preguntas:
+            1. ¿Cuál es el origen y la historia breve de esta raza?
+            2. ¿Cuál es la esperanza de vida promedio de esta raza?
+            3. ¿Cuáles son las características físicas y de comportamiento típicas de esta raza?
+            4. ¿Cuáles son las necesidades de ejercicio específicas para esta raza?
+            5. ¿Qué problemas de salud comunes pueden afectar a esta raza y cómo se pueden prevenir o manejar?
+            6. ¿Cuáles son las recomendaciones de alimentación y nutrición para esta raza, teniendo en cuenta su edad y tamaño?
+            7. ¿Qué cuidado del pelaje y aseo se recomienda para esta raza?
+            8. ¿Hay consejos específicos de socialización y entrenamiento que se deban considerar para esta raza?
 
             Output:
             """
         )
-        runnable = prompt_tratamiento | chain | StrOutputParser()
-        tratamiento = runnable.invoke({"diagnosis": diagnostico})
-        st.markdown("**Sugerencias generales de tratamiento:**")
-        st.write(tratamiento)
+        runnable = prompt_cuidado | chain | StrOutputParser()
+        cuidado = runnable.invoke({"pet_info": informacion_mascota})
+        informacion_generada += "**Recomendaciones e información extra sobre tu mascota:**\n" + cuidado
 
-        # Sugerir qué tipo de profesional médico visitar
-        prompt_profesional = PromptTemplate.from_template(
-            """
-            Basándose en la siguiente información general:
-            {diagnosis}
-            ¿Qué tipo de profesional médico sería adecuado para una consulta? Esta es solo una sugerencia y no reemplaza la consulta médica profesional.
+        st.markdown(informacion_generada)
 
-            Output:
-            """
-        )
-        runnable = prompt_profesional | chain | StrOutputParser()
-        profesional = runnable.invoke({"diagnosis": diagnostico})
-        st.markdown("**Profesional Médico que debes visitar:**")
-        st.write(profesional)
-
-# Nota: Asegúrate de tener configuradas tus claves API y cualquier otro ajuste específico necesario para tu entorno y APIs.
+# Botón para descargar la información generada
+if informacion_generada:
+    download_button(informacion_generada, "informacion_mascota.txt", "Descargar Información")
